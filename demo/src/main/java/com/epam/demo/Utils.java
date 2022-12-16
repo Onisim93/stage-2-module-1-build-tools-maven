c94c3ca142325e0f48a27d26b624e5f2370930f5
onisim_albarov <onisim93@gmail.com>
2022-12-16 19:31:14 +0400

    complete stage-2-module-1-build-tools-maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,16 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        for (String s : args) {
+            if (!StringUtils.isPositiveNumber(s)) return false;
+        }
+
+        return true;
        //magic happens here
     }
 }
