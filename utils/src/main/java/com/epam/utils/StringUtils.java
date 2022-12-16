372f6e4e4dff8b409fad0dbfb6f90a8546d075d8
onisim_albarov <onisim93@gmail.com>
2022-12-16 20:10:15 +0400

    fix2 StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -5,6 +5,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isParsable(str) && Double.parseDouble(str) > 0;
+        return NumberUtils.isParsable(str) && !str.startsWith("0") && Double.parseDouble(str) > 0;
     }
 }
