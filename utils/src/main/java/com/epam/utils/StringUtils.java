8b371f6baab9b8925cb1bcdcc552be761b9ef854
onisim_albarov <onisim93@gmail.com>
2022-12-16 20:07:11 +0400

    fix StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,9 +1,10 @@
 package com.epam.utils;
 
 import org.apache.commons.lang3.*;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Double.parseDouble(str) > 0;
+        return NumberUtils.isParsable(str) && Double.parseDouble(str) > 0;
     }
 }
